545543d51a09feea2eff31aacba68206db2190a6
sasakiyori <sasakiyori@DESKTOP-2KB6VMA>
2017-07-19 17:26:49 +0800

    first day

---
+++ src/MainInterface.java
@@ -0,0 +1,35 @@
+import java.awt.Frame;
+import java.awt.TextField;
+
+public class MainInterface extends Frame {
+
+	TextField tf;
+	
+	/**
+	 * constructor
+	 * @param
+	 * @return
+	 */
+	MainInterface() {
+		tf = new TextField();
+	}
+	
+	/**
+	 * summon the servants only once
+	 * @param
+	 * @return
+	 */
+	void singleSummon() {
+		
+	}
+	
+	
+	/**
+	 * summon the servants ten times
+	 * @param
+	 * @return
+	 */
+	void tenTimeSummon() {
+		
+	}
+}
---
+++ src/SimulatorRunner.java
@@ -0,0 +1,9 @@
+
+public class SimulatorRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new MainInterface();
+	}
+
+}
